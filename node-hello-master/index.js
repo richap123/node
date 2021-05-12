9eb7eb3726c67ca0d11cd6be85b1cef0f300ad72
richap123 <82135387+richap123@users.noreply.github.com>
2021-05-12 20:19:11 +0530

    update index.js

--- node-hello-master/index.js
+++ node-hello-master/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3010;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node! hopw you are doing well \n'
   res.end(msg);
 });
 
